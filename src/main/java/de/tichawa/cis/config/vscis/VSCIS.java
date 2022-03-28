862c1e73d3fa08502ac4ab134ed2cbc10de21750
Nightara <webmin@nightara.de>
2022-03-28 13:25:38 +0200

    Updated VSCIS and VHCIS getCLCalc method to return CameraLink object

--- src/main/java/de/tichawa/cis/config/vscis/VSCIS.java
+++ src/main/java/de/tichawa/cis/config/vscis/VSCIS.java
@@ -3,6 +3,8 @@ package de.tichawa.cis.config.vscis;
 import de.tichawa.cis.config.*;
 import de.tichawa.cis.config.model.tables.records.*;
 
+import java.util.*;
+
 public class VSCIS extends CIS
 {
 
@@ -66,113 +68,91 @@ public class VSCIS extends CIS
   }
 
   @Override
-  public String getCLCalc(int numOfPix)
+  public Optional<CameraLink> getCLCalc(int numOfPix)
   {
     int numOfPixNominal;
     int taps;
     int chipsPerTap;
-    double portDataRate;
+    long portDataRate;
     int lval;
     int pixPerTap;
     double ppsbin;
     double binning;
-    StringBuilder printOut = new StringBuilder();
+    int pixelClock = 85000000;
 
     SensorBoardRecord sensorBoard = getSensorBoard("SMARAGD").orElseThrow(() -> new CISException("Unknown sensor board"));
     SensorChipRecord sensorChip = getSensorChip("SMARAGD" + getSelectedResolution().getBoardResolution() + "_VS").orElseThrow(() -> new CISException("Unknown sensor chip"));
     numOfPixNominal = numOfPix - (getBoardCount() * sensorBoard.getOverlap() / (1200 / getSelectedResolution().getActualResolution()));
-    taps = (int) Math.ceil((numOfPix * getSelectedLineRate() / 1000000.0) / 85.0);
-    chipsPerTap = (int) Math.ceil((sensorBoard.getChips() * getBoardCount()) / (double) taps);
-    ppsbin = sensorChip.getPixelPerSensor() / ((double) getSelectedResolution().getBoardResolution() / (double) getSelectedResolution().getActualResolution());
-    pixPerTap = (int) (chipsPerTap * ppsbin);
-    portDataRate = pixPerTap * getSelectedLineRate() / 1000000.0;
 
-    while(portDataRate > 85.0)
+    taps = numOfPix * getSelectedLineRate() / pixelClock;
+    taps--;
+
+    do
     {
       taps++;
       chipsPerTap = (int) Math.ceil((sensorBoard.getChips() * getBoardCount()) / (double) taps);
       ppsbin = sensorChip.getPixelPerSensor() / ((double) getSelectedResolution().getBoardResolution() / (double) getSelectedResolution().getActualResolution());
       pixPerTap = (int) (chipsPerTap * ppsbin);
-      portDataRate = pixPerTap * getSelectedLineRate() / 1000000.0;
+      portDataRate = (long) pixPerTap * getSelectedLineRate();
     }
+    while(portDataRate > pixelClock);
+
     binning = 1 / (sensorChip.getBinning() * ((double) getSelectedResolution().getBoardResolution() / (double) getSelectedResolution().getActualResolution()));
     lval = (int) (chipsPerTap * (ppsbin - (sensorBoard.getOverlap() * binning) / sensorBoard.getChips()));
     lval -= lval % 8;
 
-    printOut.append(getString("datarate")).append(Math.round(getPhaseCount() * numOfPix * getSelectedLineRate() / 100000.0) / 10.0).append(" MByte\n");
-    printOut.append(getString("numofcons")).append((taps * getPhaseCount() > 3) ? "2" : "1").append("\n");
-    printOut.append(getString("numofport")).append(taps * getPhaseCount()).append("\n");
-    printOut.append("Pixel Clock: 85 MHz").append("\n");
-    printOut.append("Nominal pixel count: ").append(numOfPixNominal).append("\n");
+    CameraLink cl = new CameraLink(portDataRate, numOfPixNominal, pixelClock);
+    LinkedList<CameraLink.Connection> connections = new LinkedList<>();
 
-    switch(getPhaseCount())
+    if(getPhaseCount() == 3)
     {
-      case 3:
+      if(taps > 3)
       {
-        if(taps > 3)
-        {
-          System.out.println("Please select a lower line rate. Currently required number of taps (" + taps * getPhaseCount() + ") is too high.");
-          return null;
-        }
-
-        for(int x = 0; x < Math.min(2, taps); x++)
-        {
-          printOut.append("Camera Link ").append(x + 1).append(":\n");
-          printOut.append("   Port ").append(getPortName(x * 3)).append(":   ")
-                  .append(String.format("%05d", x * lval)).append("   - ").append(String.format("%05d", (x + 1) * lval - 1)).append("   ")
-                  .append(getString("Red")).append("\n");
-          printOut.append("   Port ").append(getPortName(x * 3 + 1)).append(":   ")
-                  .append(String.format("%05d", x * lval)).append("   - ").append(String.format("%05d", (x + 1) * lval - 1)).append("   ")
-                  .append(getString("Green")).append("\n");
-          printOut.append("   Port ").append(getPortName(x * 3 + 2)).append(":   ")
-                  .append(String.format("%05d", x * lval)).append("   - ").append(String.format("%05d", (x + 1) * lval - 1)).append("   ")
-                  .append(getString("Blue")).append("\n");
-        }
-
-        if(taps == 3)
-        {
-          printOut.append("   Port ").append(getPortName(3)).append(":   ")
-                  .append(String.format("%05d", 2 * lval)).append("   - ").append(String.format("%05d", 3 * lval - 1)).append("   ")
-                  .append(getString("Red")).append("\n");
-          printOut.append("   Port ").append(getPortName(4)).append(":   ")
-                  .append(String.format("%05d", 2 * lval)).append("   - ").append(String.format("%05d", 3 * lval - 1)).append("   ")
-                  .append(getString("Green")).append("\n");
-          printOut.append("   Port ").append(getPortName(5)).append(":   ")
-                  .append(String.format("%05d", 2 * lval)).append("   - ").append(String.format("%05d", 3 * lval - 1)).append("   ")
-                  .append(getString("Blue")).append("\n");
-        }
-
-        break;
+        System.out.println("Please select a lower line rate. Currently required number of taps (" + taps * getPhaseCount() + ") is too high.");
+        return Optional.empty();
       }
-      case 1:
+
+      for(int x = 0; x < Math.min(2, taps); x++)
       {
-        if(taps > 8)
-        {
-          System.out.println("Please select a lower line rate. Currently required number of taps (" + taps * getPhaseCount() + ") is too high.");
-          return null;
-        }
+        connections.add(new CameraLink.Connection());
+        connections.getLast().addPorts(new CameraLink.Port(x * lval,(x + 1) * lval - 1,"Red"),
+            new CameraLink.Port(x * lval,(x + 1) * lval - 1,"Green"),
+            new CameraLink.Port(x * lval,(x + 1) * lval - 1,"Blue"));
+      }
 
-        printOut.append("Camera Link 1:\n");
-        for(int x = 0; x < Math.min(3, taps); x++)
-        {
-          printOut.append("   Port ").append(getPortName(x)).append(":   ")
-                  .append(String.format("%05d", x * lval)).append("   - ").append(String.format("%05d", (x + 1) * lval - 1)).append("\n");
-        }
+      if(taps == 3)
+      {
+        connections.getLast().addPorts(new CameraLink.Port(2 * lval, (2 + 1) * lval - 1, "Red"),
+            new CameraLink.Port(2 * lval, (2 + 1) * lval - 1, "Green"),
+            new CameraLink.Port(2 * lval, (2 + 1) * lval - 1, "Blue"));
+      }
+    }
+    else if(getPhaseCount() == 1)
+    {
+      if(taps > 8)
+      {
+        System.out.println("Please select a lower line rate. Currently required number of taps (" + taps * getPhaseCount() + ") is too high.");
+        return Optional.empty();
+      }
 
-        if(taps > 3)
+      connections.add(new CameraLink.Connection());
+      for(int x = 0; x < Math.min(3, taps); x++)
+      {
+        connections.getLast().addPorts(new CameraLink.Port(x * lval, (x + 1) * lval - 1));
+      }
+
+      if(taps > 3)
+      {
+        connections.add(new CameraLink.Connection());
+        for(int x = 3; x < taps; x++)
         {
-          printOut.append("Camera Link 2:\n");
-          for(int x = 3; x < taps; x++)
-          {
-            printOut.append("   Port ").append(getPortName(x)).append(":   ")
-                    .append(String.format("%05d", x * lval)).append("   - ").append(String.format("%05d", (x + 1) * lval - 1)).append("\n");
-          }
+          connections.getLast().addPorts(new CameraLink.Port(x * lval, (x + 1) * lval - 1));
         }
-        break;
       }
     }
 
-    return printOut.toString();
+    connections.forEach(cl::addConnection);
+    return Optional.of(cl);
   }
 
   @Override
