36048015557c895e8d4a49e49ee0af83fcd5aeea
Leonelle Kommegne <l.kommegne@tichawa.de>
2022-10-25 12:00:26 +0200

    added default table sorting, also auto-formatting -kba

--- src/main/java/de/tichawa/cis/config/CalculationController.java
+++ src/main/java/de/tichawa/cis/config/CalculationController.java
@@ -1,8 +1,5 @@
 package de.tichawa.cis.config;
 
-import java.net.*;
-import java.text.*;
-import java.util.*;
 import javafx.beans.property.*;
 import javafx.collections.*;
 import javafx.fxml.*;
@@ -11,14 +8,17 @@ import javafx.print.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.scene.transform.*;
-import javafx.stage.*;
+import javafx.scene.transform.Scale;
+import javafx.stage.Stage;
 
-public class CalculationController implements Initializable
-{
-  
+import java.net.URL;
+import java.text.*;
+import java.util.*;
+
+public class CalculationController implements Initializable {
+
   protected CIS CIS_DATA;
-  
+
   @FXML
   private Label Header;
   @FXML
@@ -39,36 +39,32 @@ public class CalculationController implements Initializable
   private Menu Lang;
   @FXML
   private MenuItem SwitchLang;
-  
+
   @Override
-  public void initialize(URL url, ResourceBundle rb)
-  {
-    
+  public void initialize(URL url, ResourceBundle rb) {
+
   }
-  
-  public void passData(CIS data)
-  {
+
+  public void passData(CIS data) {
     this.CIS_DATA = data;
-    
+
     load();
   }
-  
+
   @SuppressWarnings("unchecked")
-  private void load()
-  {
-    try
-    {
+  private void load() {
+    try {
       Lang.setText(ResourceBundle.getBundle("de.tichawa.cis.config.Bundle", CIS_DATA.getLocale()).getString("lang"));
       SwitchLang.setText(ResourceBundle.getBundle("de.tichawa.cis.config.Bundle", CIS_DATA.getLocale()).getString("switchlang"));
-      
+
       File.setText(ResourceBundle.getBundle("de.tichawa.cis.config.Bundle", CIS_DATA.getLocale()).getString("File"));
       Print.setText(ResourceBundle.getBundle("de.tichawa.cis.config.Bundle", CIS_DATA.getLocale()).getString("Print"));
-      
+
       NumberFormat f = NumberFormat.getInstance(CIS_DATA.getLocale());
       String[] calcText = CIS_DATA.createCalculation().split("\n\t\n");
-      
+
       Header.setText(calcText[0]);
-      
+
       String electData = calcText[1].substring(calcText[1].indexOf("\n\n") + 2);
       String[] headers = electData.split("\n")[0].split("\t");
       TableColumn<CalcLine, String> electNameCol = new TableColumn<>(headers[0]);
@@ -104,20 +100,20 @@ public class CalculationController implements Initializable
       electPowerCol.setCellFactory(c -> new CustomDoubleCell());
       electPowerCol.setMinWidth(150.0);
       electPowerCol.setMaxWidth(150.0);
-      
+
       ObservableList<CalcLine> electDataList = FXCollections.observableArrayList();
-      
+
       electData = electData.substring(electData.indexOf("\n") + 1);
-      for(String row : electData.split("\n"))
-      {
+      for (String row : electData.split("\n")) {
         String[] data = row.split("\t");
         electDataList.add(new CalcLine(data[0], data[1], f.parse(data[2]).intValue(), f.parse(data[3]).doubleValue(), f.parse(data[4]).doubleValue(), f.parse(data[5]).doubleValue(), f.parse(data[6]).doubleValue()));
       }
-      
+
       Electronics.setItems(electDataList);
       Electronics.getColumns().clear();
       Electronics.getColumns().addAll(electNameCol, electIdCol, electAmountCol, electPriceCol, electWeightCol, electTimeCol, electPowerCol);
-      
+      Electronics.getSortOrder().add(electNameCol);
+
       String eTotalData = calcText[2];
       TableColumn<CalcLine, String> eTotalNameCol = new TableColumn<>(headers[0]);
       eTotalNameCol.setCellValueFactory(cellData -> cellData.getValue().getName());
@@ -151,13 +147,13 @@ public class CalculationController implements Initializable
       eTotalPowerCol.setCellFactory(c -> new CustomDoubleCell());
       eTotalPowerCol.setMinWidth(150.0);
       eTotalPowerCol.setMaxWidth(150.0);
-      
+
       String[] eData = eTotalData.split("\t");
       ElectSubTotals.getItems().clear();
       ElectSubTotals.getItems().add(new CalcLine(eData[0], eData[1], f.parse(eData[2]).intValue(), f.parse(eData[3]).doubleValue(), f.parse(eData[4]).doubleValue(), f.parse(eData[5]).doubleValue(), f.parse(eData[6]).doubleValue()));
       ElectSubTotals.getColumns().clear();
       ElectSubTotals.getColumns().addAll(eTotalNameCol, eTotalIdCol, eTotalAmountCol, eTotalPriceCol, eTotalWeightCol, eTotalTimeCol, eTotalPowerCol);
-      
+
       String mechaData = calcText[3].substring(calcText[3].indexOf("\n\n") + 2);
       TableColumn<CalcLine, String> mechaNameCol = new TableColumn<>(headers[0]);
       mechaNameCol.setCellValueFactory(cellData -> cellData.getValue().getName());
@@ -182,20 +178,20 @@ public class CalculationController implements Initializable
       mechaWeightCol.setCellFactory(c -> new CustomDoubleCell());
       mechaWeightCol.setMinWidth(200.0);
       mechaWeightCol.setMaxWidth(200.0);
-      
+
       ObservableList<CalcLine> mechaDataList = FXCollections.observableArrayList();
-      
+
       mechaData = mechaData.substring(mechaData.indexOf("\n") + 1);
-      for(String row : mechaData.split("\n"))
-      {
+      for (String row : mechaData.split("\n")) {
         String[] data = row.split("\t");
         mechaDataList.add(new CalcLine(data[0], data[1], f.parse(data[2]).intValue(), f.parse(data[3]).doubleValue(), f.parse(data[4]).doubleValue(), 0.0, 0.0));
       }
-      
+
       Mechanics.setItems(mechaDataList);
       Mechanics.getColumns().clear();
       Mechanics.getColumns().addAll(mechaNameCol, mechaIdCol, mechaAmountCol, mechaPriceCol, mechaWeightCol);
-      
+      Mechanics.getSortOrder().add(mechaNameCol);
+
       String mTotalData = calcText[4];
       TableColumn<CalcLine, String> mTotalNameCol = new TableColumn<>(headers[0]);
       mTotalNameCol.setCellValueFactory(cellData -> cellData.getValue().getName());
@@ -219,13 +215,13 @@ public class CalculationController implements Initializable
       mTotalWeightCol.setCellFactory(c -> new CustomDoubleCell());
       mTotalWeightCol.setMinWidth(200.0);
       mTotalWeightCol.setMaxWidth(200.0);
-      
+
       String[] mData = mTotalData.split("\t");
       MechaSubTotals.getItems().clear();
       MechaSubTotals.getItems().add(new CalcLine(mData[0], mData[1], f.parse(mData[2]).intValue(), f.parse(mData[3]).doubleValue(), f.parse(mData[4]).doubleValue(), 0.0, 0.0));
       MechaSubTotals.getColumns().clear();
       MechaSubTotals.getColumns().addAll(mTotalNameCol, mTotalIdCol, mTotalAmountCol, mTotalPriceCol, mTotalWeightCol);
-      
+
       String totalData = calcText[5].substring(calcText[5].indexOf("\n\n") + 2);
       TableColumn<PriceLine, String> totalNameCol = new TableColumn<>(headers[0]);
       totalNameCol.setCellValueFactory(cellData -> cellData.getValue().getName());
@@ -247,28 +243,22 @@ public class CalculationController implements Initializable
       totalWeightCol.setCellValueFactory(cellData -> cellData.getValue().getWeight());
       totalWeightCol.setMinWidth(200.0);
       totalWeightCol.setMaxWidth(200.0);
-      
+
       ObservableList<PriceLine> totalDataList = FXCollections.observableArrayList();
-      
+
       totalData = totalData.substring(totalData.indexOf("\n") + 1);
-      for(String row : totalData.split("\n"))
-      {
+      for (String row : totalData.split("\n")) {
         String[] data = row.split("\t");
         totalDataList.add(new PriceLine(data[0], data[1], data[2], data[3], data[4]));
       }
-      
-      Totals.setRowFactory(row -> new TableRow<PriceLine>()
-      {
+
+      Totals.setRowFactory(row -> new TableRow<PriceLine>() {
         @Override
-        public void updateIndex(int index)
-        {
+        public void updateIndex(int index) {
           super.updateIndex(index);
-          if(index == Totals.getItems().size() - 1)
-          {
+          if (index == Totals.getItems().size() - 1) {
             getStyleClass().add("table-bold");
-          }
-          else
-          {
+          } else {
             getStyleClass().remove("table-bold");
           }
         }
@@ -276,7 +266,7 @@ public class CalculationController implements Initializable
       Totals.setItems(totalDataList);
       Totals.getColumns().clear();
       Totals.getColumns().addAll(totalNameCol, totalIdCol, totalAmountCol, totalPriceCol, totalWeightCol);
-      
+
       ElectSubTotals.getStyleClass().add("table-view-hidden");
       ElectSubTotals.getStyleClass().add("table-bold");
       ElectSubTotals.fixedCellSizeProperty().set(20.0);
@@ -284,61 +274,53 @@ public class CalculationController implements Initializable
       MechaSubTotals.getStyleClass().add("table-bold");
       MechaSubTotals.fixedCellSizeProperty().set(20.0);
       Totals.getStyleClass().add("table-view-hidden");
-      
+
       Pane eHeader = (Pane) ElectSubTotals.lookup("TableHeaderRow");
       eHeader.setVisible(false);
       ElectSubTotals.setLayoutY(-eHeader.getHeight());
       ElectSubTotals.autosize();
       ElectSubTotals.scrollTo(0);
-      
+
       Pane mHeader = (Pane) MechaSubTotals.lookup("TableHeaderRow");
       mHeader.setVisible(false);
       MechaSubTotals.setLayoutY(-mHeader.getHeight());
       MechaSubTotals.autosize();
       MechaSubTotals.scrollTo(0);
-      
+
       Pane tHeader = (Pane) Totals.lookup("TableHeaderRow");
       tHeader.setVisible(false);
       Totals.setLayoutY(-tHeader.getHeight());
       Totals.autosize();
-    }
-    catch(CISException e)
-    {
+    } catch (CISException e) {
       Alert alert = new Alert(Alert.AlertType.ERROR);
       alert.setHeaderText(e.getMessage());
       alert.show();
+    } catch (ParseException | NullPointerException ignored) {
     }
-    catch(ParseException | NullPointerException ignored)
-    {}
   }
-  
-  public void print()
-  {
+
+  public void print() {
     String calc = CIS_DATA.getVersionHeader() + "\n\t\n" + CIS_DATA.createCalculation();
     Stage printStage = new Stage();
     GridPane printPane = new GridPane();
     printPane.getStylesheets().add("/de/tichawa/cis/config/style.css");
     printPane.getStyleClass().add("white");
     Scene printScene = new Scene(printPane);
-    
+
     int x = 0;
-    for(String row : calc.split("\n"))
-    {
+    for (String row : calc.split("\n")) {
       int y = 0;
-      
-      if(row.isEmpty()) {
+
+      if (row.isEmpty()) {
         continue;
       }
-      
-      if(row.equals("\t"))
-      {
+
+      if (row.equals("\t")) {
         row = " ";
       }
-      
-      for(String data : row.split("\t"))
-      {
-        if(!data.isEmpty())
-        {
+
+      for (String data : row.split("\t")) {
+        if (!data.isEmpty()) {
           StackPane p = new StackPane();
           p.setMinHeight(20.0);
           p.setMaxHeight(20.0);
@@ -346,57 +328,49 @@ public class CalculationController implements Initializable
           p.setAlignment(Pos.CENTER_LEFT);
           p.getChildren().add(new Label(data));
           p.getStyleClass().add("bolder");
-          if(x % 2 == 0)
-          {
+          if (x % 2 == 0) {
             p.getStyleClass().add("pale-blue");
-          }
-          else
-          {
+          } else {
             p.getStyleClass().add("white");
           }
-          
+
           printPane.add(p, y, x);
           y++;
         }
       }
       x++;
     }
-    
+
     printStage.setScene(printScene);
     printStage.show();
-    
+
     PrinterJob p = PrinterJob.createPrinterJob();
-    
-    if(p.showPrintDialog(null))
-    {
+
+    if (p.showPrintDialog(null)) {
       p.getJobSettings().setPageLayout(p.getPrinter().createPageLayout(Paper.A4, PageOrientation.PORTRAIT, Printer.MarginType.EQUAL_OPPOSITES));
       double scaleX = p.getJobSettings().getPageLayout().getPrintableWidth() / printPane.getWidth();
       double scaleY = p.getJobSettings().getPageLayout().getPrintableHeight() / (getRowCount(printPane) * 20.0);
 
       printPane.getTransforms().add(new Scale(Math.min(scaleX, scaleY), Math.min(scaleX, scaleY)));
-      
-      if(p.printPage(printPane))
-      {
+
+      if (p.printPage(printPane)) {
         printPane.getTransforms().clear();
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setHeaderText(ResourceBundle.getBundle("de.tichawa.cis.config.Bundle", CIS_DATA.getLocale()).getString("printsuccess"));
         alert.show();
         p.endJob();
-      }
-      else
-      {
+      } else {
         printPane.getTransforms().clear();
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setHeaderText(ResourceBundle.getBundle("de.tichawa.cis.config.Bundle", CIS_DATA.getLocale()).getString("A fatal error occurred during the printing attempt.Please control your print settings."));
         alert.show();
       }
     }
-    
+
     printStage.close();
   }
-  
-  public static class CalcLine
-  {
+
+  public static class CalcLine {
     private final SimpleStringProperty name;
     private final SimpleStringProperty id;
     private final SimpleIntegerProperty amount;
@@ -404,9 +378,8 @@ public class CalculationController implements Initializable
     private final SimpleDoubleProperty weight;
     private final SimpleDoubleProperty time;
     private final SimpleDoubleProperty power;
-    
-    public CalcLine(String name, String id, int amount, double price, double weight, double time, double power)
-    {
+
+    public CalcLine(String name, String id, int amount, double price, double weight, double time, double power) {
       this.name = new SimpleStringProperty(name);
       this.id = new SimpleStringProperty(id);
       this.amount = new SimpleIntegerProperty(amount);
@@ -415,136 +388,108 @@ public class CalculationController implements Initializable
       this.time = new SimpleDoubleProperty(time);
       this.power = new SimpleDoubleProperty(power);
     }
-    
-    public SimpleStringProperty getName()
-    {
+
+    public SimpleStringProperty getName() {
       return name;
     }
-    
-    public SimpleStringProperty getId()
-    {
+
+    public SimpleStringProperty getId() {
       return id;
     }
-    
-    public SimpleIntegerProperty getAmount()
-    {
+
+    public SimpleIntegerProperty getAmount() {
       return amount;
     }
-    
-    public SimpleDoubleProperty getPrice()
-    {
+
+    public SimpleDoubleProperty getPrice() {
       return price;
     }
-    
-    public SimpleDoubleProperty getWeight()
-    {
+
+    public SimpleDoubleProperty getWeight() {
       return weight;
     }
-    
-    public SimpleDoubleProperty getTime()
-    {
+
+    public SimpleDoubleProperty getTime() {
       return time;
     }
-    
-    public SimpleDoubleProperty getPower()
-    {
+
+    public SimpleDoubleProperty getPower() {
       return power;
     }
-    
+
   }
-  
-  public static class PriceLine
-  {
+
+  public static class PriceLine {
     private final SimpleStringProperty name;
     private final SimpleStringProperty id;
     private final SimpleStringProperty amount;
     private final SimpleStringProperty price;
     private final SimpleStringProperty weight;
-    
-    public PriceLine(String name, String id, String amount, String price, String weight)
-    {
+
+    public PriceLine(String name, String id, String amount, String price, String weight) {
       this.name = new SimpleStringProperty(name);
       this.id = new SimpleStringProperty(id);
       this.amount = new SimpleStringProperty(amount);
       this.price = new SimpleStringProperty(price);
       this.weight = new SimpleStringProperty(weight);
     }
-    
-    public SimpleStringProperty getName()
-    {
+
+    public SimpleStringProperty getName() {
       return name;
     }
-    
-    public SimpleStringProperty getId()
-    {
+
+    public SimpleStringProperty getId() {
       return id;
     }
-    
-    public SimpleStringProperty getAmount()
-    {
+
+    public SimpleStringProperty getAmount() {
       return amount;
     }
-    
-    public SimpleStringProperty getPrice()
-    {
+
+    public SimpleStringProperty getPrice() {
       return price;
     }
-    
-    public SimpleStringProperty getWeight()
-    {
+
+    public SimpleStringProperty getWeight() {
       return weight;
     }
   }
-  
-  private class CustomIntCell extends TableCell<CalcLine, Integer>
-  {
+
+  private class CustomIntCell extends TableCell<CalcLine, Integer> {
     @Override
-    public void updateItem(final Integer item, boolean empty)
-    {
-      if(item != null)
-      {
+    public void updateItem(final Integer item, boolean empty) {
+      if (item != null) {
         setText(String.format(CIS_DATA.getLocale(), "%d", item));
       }
     }
   }
-  
-  private class CustomDoubleCell extends TableCell<CalcLine, Double>
-  {
+
+  private class CustomDoubleCell extends TableCell<CalcLine, Double> {
     @Override
-    public void updateItem(final Double item, boolean empty)
-    {
-      if(item != null)
-      {
+    public void updateItem(final Double item, boolean empty) {
+      if (item != null) {
         setText(String.format(CIS_DATA.getLocale(), "%.2f", item));
       }
     }
   }
-  
-  public void switchLang()
-  {
-    if(CIS_DATA.getLocale().toString().equals("de_DE"))
-    {
+
+  public void switchLang() {
+    if (CIS_DATA.getLocale().toString().equals("de_DE")) {
       CIS.setLocale(new Locale("en", "US"));
-    }
-    else if(CIS_DATA.getLocale().toString().equals("en_US"))
-    {
+    } else if (CIS_DATA.getLocale().toString().equals("en_US")) {
       CIS.setLocale(new Locale("de", "DE"));
     }
-    
+
     load();
   }
-  
-  private int getRowCount(GridPane pane)
-  {
+
+  private int getRowCount(GridPane pane) {
     int numRows = pane.getRowConstraints().size();
-    for(int i = 0; i < pane.getChildren().size(); i++)
-    {
+    for (int i = 0; i < pane.getChildren().size(); i++) {
       Node child = pane.getChildren().get(i);
-      if (child.isManaged())
-      {
+      if (child.isManaged()) {
         Integer rowIndex = GridPane.getRowIndex(child);
-        if(rowIndex != null)
-        {
+        if (rowIndex != null) {
           numRows = Math.max(numRows, rowIndex + 1);
         }
       }
